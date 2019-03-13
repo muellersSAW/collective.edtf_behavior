c61302e9b217f0c37d30b029d52f13f64213442d
MrTango <md@derico.de>
2019-03-13 19:04:38 +0200

    finsh indexers and add tests for indexes

--- src/collective/edtf_behavior/indexers/edtf.py
+++ src/collective/edtf_behavior/indexers/edtf.py
@@ -3,6 +3,7 @@ from collective.edtf_behavior.behaviors.edtf_date import IEDTFDateMarker
 from collective.edtf_behavior.behaviors.edtf_date import IEDTFDate
 from plone.dexterity.interfaces import IDexterityContent
 from plone.indexer import indexer
+from zope.interface import Interface
 
 
 @indexer(IDexterityContent)
@@ -12,28 +13,32 @@ def dummy(obj):
 
 
 @indexer(IEDTFDateMarker)
-def edtf_date(obj):
-    print(obj.edtf_date)
-    return obj.edtf_date
-
-
-@indexer(IDexterityContent)
-def date_earliest(obj):
-    print(obj.date_earliest)
-    return obj.date_earliest
-
-
-@indexer(IEDTFDate)
 def date_latest(obj):
-    print(obj.date_latest)
-    return obj.date_latest
+    adapted_obj = IEDTFDate(obj)
+    if not adapted_obj:
+        return
+    return adapted_obj.date_latest
+
+
+@indexer(IEDTFDateMarker)
+def date_earliest(obj):
+    adapted_obj = IEDTFDate(obj)
+    if not adapted_obj:
+        return
+    return adapted_obj.date_earliest
 
 
 @indexer(IEDTFDateMarker)
 def date_sort_ascending(obj):
-    return obj.date_sort_ascending
+    adapted_obj = IEDTFDate(obj)
+    if not adapted_obj:
+        return
+    return adapted_obj.date_sort_ascending
 
 
 @indexer(IEDTFDateMarker)
 def date_sort_descending(obj):
-    return obj.date_sort_descending
+    adapted_obj = IEDTFDate(obj)
+    if not adapted_obj:
+        return
+    return adapted_obj.date_sort_descending
--- src/collective/edtf_behavior/testing.py
+++ src/collective/edtf_behavior/testing.py
@@ -18,8 +18,6 @@ class CollectiveEdtfBehaviorLayer(PloneSandboxLayer):
         # Load any other ZCML that is required for your tests.
         # The z3c.autoinclude feature is disabled in the Plone fixture base
         # layer.
-        import plone.restapi
-        self.loadZCML(package=plone.restapi)
         self.loadZCML(package=collective.edtf_behavior)
 
     def setUpPloneSite(self, portal):
