23e824aa4b9629337db93472cfbad62341565fb0
MrTango <md@derico.de>
2019-03-22 19:51:48 +0200

    Back to development: 1.0.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.edtf_behavior',
-    version='1.0.2',
+    version='1.0.3.dev0',
     description="A EDTF behavior for Plone.",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
