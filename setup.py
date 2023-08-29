fa99b6cd8beb60ae63f97be71db54069192d05ea
muellers-saw-leipzig <muellers@saw-leipzig.de>
2023-08-29 15:01:23 +0200

    fixed wrong python_requires statement (comma is treated as logical AND)

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
@@ -40,7 +41,7 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7, >=3.7",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
     install_requires=[
         'setuptools',
         # -*- Extra requirements: -*-
